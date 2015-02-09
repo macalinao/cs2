1386ebd4d0009d36e31792a4b6e4657c2c279a57
Ian Macalinao <me@ian.pw>
2015-02-09 17:52:25 -0600

    Add binary search

---
+++ 2015-02-09/BinarySearch.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class BinarySearch {
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    System.out.println(find(arr, 6) + " should be " + 5);
+  }
+
+  public static int find(int[] arr, int val) {
+    int lo = 0;
+    int hi = arr.length - 1;
+    for (;;) {
+      if (hi == lo) return -1;
+      int mid = lo + (hi - lo) / 2;
+      int v = arr[mid];
+      if (val == v) return mid;
+      if (val > v) {
+        lo = mid;
+      } else if (val < v) {
+        hi = mid;
+      }
+    }
+  }
+}
