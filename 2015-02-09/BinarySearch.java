5f71d9377d3b0e83448a0027e04ea82b22ad637e
Ian Macalinao <me@ian.pw>
2015-02-09 17:52:40 -0600

    tesT

--- 2015-02-09/BinarySearch.java
+++ 2015-02-09/BinarySearch.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class BinarySearch {
   public static void main(String[] args) {
-    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
     System.out.println(find(arr, 6) + " should be " + 5);
   }
 
