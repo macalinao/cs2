794a7c099d95274e5c6a372f2c01772b08d95e5e
Ian Macalinao <me@ian.pw>
2015-02-09 18:01:13 -0600

    Print comparisons better comparison

--- 2015-02-09/BinarySearch.java
+++ 2015-02-09/BinarySearch.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class BinarySearch {
   public static void main(String[] args) {
     int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    System.out.println(find(arr, 2) + " should be " + 1);
+    System.out.println(find(arr, 11) + " should be " + 10);
   }
 
   public static int find(int[] arr, int val) {
--- 2015-02-09/LinearSearch.java
+++ 2015-02-09/LinearSearch.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class LinearSearch {
   public static void main(String[] args) {
-    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-    System.out.println(find(arr, 6) + " should be " + 5);
+    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    System.out.println(find(arr, 11) + " should be " + 10);
   }
 
   public static int find(int[] arr, int val) {
