609e87bc5157f5de9ec81b9367b165ee7630f509
Ian Macalinao <me@ian.pw>
2015-02-09 18:00:15 -0600

    Comparison count

--- 2015-02-09/BinarySearch.java
+++ 2015-02-09/BinarySearch.java
@@ -3,18 +3,23 @@ import java.util.*;
 public class BinarySearch {
   public static void main(String[] args) {
     int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    System.out.println(find(arr, 6) + " should be " + 5);
+    System.out.println(find(arr, 2) + " should be " + 1);
   }
 
   public static int find(int[] arr, int val) {
+    int comps = 0;
+
     int lo = 0;
     int hi = arr.length - 1;
     for (;;) {
       if (hi == lo) return -1;
-      int mid = lo + (hi - lo) / 2;
+      int mid = (lo + hi) / 2;
       int v = arr[mid];
-      if (val == v) return mid;
-      if (val > v) {
+      comps++;
+      System.out.println(comps);
+      if (val == v) {
+        return mid;
+      } else if (val > v) {
         lo = mid;
       } else if (val < v) {
         hi = mid;
--- 2015-02-09/LinearSearch.java
+++ 2015-02-09/LinearSearch.java
@@ -7,8 +7,10 @@ public class LinearSearch {
   }
 
   public static int find(int[] arr, int val) {
+    int comps = 0;
     for (int i = 0; i < arr.length; i++) {
       if (arr[i] == val) return i;
+      System.out.println(++comps);
     }
     return -1;
   }
