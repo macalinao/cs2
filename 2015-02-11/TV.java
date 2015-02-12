ba3b943f51c6b42c410a5d6b190d7e0a3056e669
Ian Macalinao <me@ian.pw>
2015-02-11 18:22:55 -0600

    aspect ratio

--- 2015-02-11/TV.java
+++ 2015-02-11/TV.java
@@ -1,19 +1,42 @@
 import java.util.*;
 
 public class TV {
-  int channel;
-  int volume;
+  static int instances = 0;
+
+  static final TV FULL_HD = new TV(1920, 1080);
+  static final TV FOURK = new TV(3840, 2160);
+
+  int channel = 1;
+  int volume = 10;
   int xRes;
   int yRes;
 
   public TV() {
-    channel = 1;
-    volume = 10;
-    xRes = 1280;
-    yRes = 720;
+    this(1280, 720);
+  }
+
+  public TV(int xRes, int yRes) {
+    this.xRes = xRes;
+    this.yRes = yRes;
+    instances++;
   }
 
   public int pixelCount() {
     return xRes * yRes;
+  }
+
+  public String aspectRatio() {
+    int gcd = gcd(xRes, yRes);
+    return (xRes / gcd) + ":" + (yRes / gcd);
+  }
+
+  public static int gcd(int a, int b) {
+    return (b == 0) ? a : gcd(b, a % b);
+  }
+
+  public static void main(String[] args) {
+    System.out.println("A full HD TV has " + FULL_HD.pixelCount() + " pixels and an aspect ratio of " + FULL_HD.aspectRatio() + ".");
+    System.out.println("A 4K TV has " + FOURK.pixelCount() + " pixels and an aspect ratio of " + FOURK.aspectRatio() + ".");
+    System.out.println("There are " + instances + " instances of TVs floating around.");
   }
 }
