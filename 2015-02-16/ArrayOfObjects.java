28bc54bd57ed17329f90cb4597bd6c863f9a29b2
Ian Macalinao <me@ian.pw>
2015-02-16 17:56:17 -0600

    stuff

--- 2015-02-16/ArrayOfObjects.java
+++ 2015-02-16/ArrayOfObjects.java
@@ -1,7 +1,23 @@
+import java.util.*;
+
 public class ArrayOfObjects {
 
   public static void main(String[] args) {
-    Object[] objs = new Object[10];
-    System.out.println(objs.length);
+    Circle[] objs = new Circle[10];
+    objs[1] = new Circle(10);
+
+    System.out.println(objs[1].getArea());
+  }
+
+  static class Circle {
+    public double radius;
+
+    public Circle(double radius) {
+      this.radius = radius;
+    }
+
+    public double getArea() {
+      return Math.PI * radius * radius;
+    }
   }
 }
