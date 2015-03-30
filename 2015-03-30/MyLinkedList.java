8e84ba817ea1c3746e59c8d0f2802e94d46d8ab3
Ian Macalinao <me@ian.pw>
2015-03-30 18:01:03 -0500

    Finish linked list

--- 2015-03-30/MyLinkedList.java
+++ 2015-03-30/MyLinkedList.java
@@ -10,11 +10,33 @@ public class MyLinkedList<T> {
     l.add("time");
 
     System.out.println(l.toString());
+
+    System.out.println(l.removeFirst());
+    System.out.println(l.removeLast());
+
+    System.out.println(l.toString());
+
+    l.addFirst("nutter");
+    l.addLast("hour");
+
+    System.out.println(l.toString());
+
+    System.out.println(l.getFirst());
+    System.out.println(l.getLast());
+
+    System.out.println(new MyLinkedList<String>(new String[]{"this", "is", "a", "test"}));
+
   }
 
-  Node head;
+  Node<T> head;
 
   public MyLinkedList() {
+  }
+
+  public MyLinkedList(T[] els) {
+    for (T e : els) {
+      add(e);
+    }
   }
 
   public void add(T val) {
@@ -24,19 +46,72 @@ public class MyLinkedList<T> {
       return;
     }
 
-    Node last = head;
+    Node<T> last = head;
     while (last.next != null) {
       last = last.next;
     }
     last.next = n;
   }
 
+  public void addFirst(T val) {
+    Node<T> n = new Node<>(val);
+    if (head == null) {
+      head = n;
+      return;
+    }
+
+    n.next = head;
+    head = n;
+  }
+
+  public void addLast(T val) {
+    add(val);
+  }
+
+  public T getFirst() {
+    if (head == null) return null;
+    return head.val;
+  }
+
+  public T getLast() {
+    if (head == null) return null;
+    Node<T> last = head;
+    while (last.next != null) {
+      last = last.next;
+    }
+    return last.val;
+  }
+
+  public T removeFirst() {
+    if (head == null) return null;
+    Node<T> pop = head;
+    head = pop.next;
+    return pop.val;
+  }
+
+  public T removeLast() {
+    if (head == null) return null;
+    if (head.next == null) {
+      T val = head.val;
+      head = null;
+      return val;
+    }
+    Node<T> last = head;
+    while (last.next.next != null) {
+      last = last.next;
+    }
+    Node<T> ret = last.next;
+    last.next = null;
+    return ret.val;
+  }
+
+  @Override
   public String toString() {
     if (head == null) {
       return "null";
     }
 
-    Node last = head;
+    Node<T> last = head;
     StringBuilder sb = new StringBuilder("[").append(last.val.toString());
     while (last.next != null) {
       last = last.next;
@@ -47,7 +122,7 @@ public class MyLinkedList<T> {
 
   static class Node<T> {
     T val;
-    Node next;
+    Node<T> next;
 
     public Node(T val) {
       this.val = val;
